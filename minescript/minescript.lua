910da6280ee1018d20a7445aa3ebf7f8572dbe8d
semyon422 <semyon422@users.noreply.github.com>
2024-01-09 17:41:36 +0500

    Upload files

---
+++ minescript/minescript.lua
@@ -0,0 +1,202 @@
+local msrt = require("msrt")
+
+local minescript = {}
+
+---Sends the given message to the chat.
+---@param message any
+function minescript.echo(message)
+	msrt.print_err(message)
+end
+
+---Gets the local player's name.
+---@async
+---@return string
+function minescript.player_name()
+	return msrt.call_async("player_name")
+end
+
+---Gets the local player's position.
+---@async
+---@return table
+function minescript.player_position()
+	return msrt.call_async("player_position")
+end
+
+---Sets the player's position, and optionally orientation.
+---@async
+---@return boolean
+function minescript.player_set_position(x, y, z, yaw, pitch)
+	return msrt.call_async("player_set_position", x, y, z, yaw, pitch)
+end
+
+---Gets the items in the local player's hands.
+---@async
+---@return table
+function minescript.player_hand_items()
+	return msrt.call_async("player_hand_items")
+end
+
+---Gets the items in the local player's inventory.
+---@async
+---@return table
+function minescript.player_inventory()
+	return msrt.call_async("player_inventory")
+end
+
+---Swaps an inventory item into the hotbar.
+---@async
+---@param slot number
+function minescript.player_inventory_slot_to_hotbar(slot)
+	msrt.call_async("player_inventory_slot_to_hotbar", slot)
+end
+
+---Selects the given slot within the player's hotbar.
+---@async
+---@param slot number
+function minescript.player_inventory_select_slot(slot)
+	msrt.call_async("player_inventory_select_slot", slot)
+end
+
+---Simulating press/release.
+---@async
+---@param key "forward" | "backward" | "left" | "right" | "jump" | "sprint" | "sneak" | "pick_item" | "use" | "attack" | "swap_hands" | "drop"
+---@param pressed boolean
+function minescript.player_press(key, pressed)
+	msrt.call_async("player_press_" .. key, pressed)
+end
+
+---Gets the local player's orientation.
+---@async
+---@return number
+function minescript.player_orientation()
+	return msrt.call_async("player_orientation")
+end
+
+---Gets the local player's orientation.
+---@async
+---@param yaw number
+---@param pitch number
+function minescript.player_set_orientation(yaw, pitch)
+	msrt.call_async("player_set_orientation", yaw, pitch)
+end
+
+---Gets info about the nearest block, if any, in the local player's crosshairs.
+---@async
+---@param max_distance number
+---@return table
+function minescript.player_get_targeted_block(max_distance)
+	return msrt.call_async("player_get_targeted_block", max_distance or 20)
+end
+
+---Gets the local player's health.
+---@async
+---@return number
+function minescript.player_health()
+	return msrt.call_async("player_health")
+end
+
+---Gets a list of nearby players and their attributes.
+---@async
+---@param nbt boolean
+---@return table
+function minescript.players(nbt)
+	return msrt.call_async("players", nbt)
+end
+
+---Gets a list of nearby entities and their attributes.
+---@async
+---@param nbt boolean
+---@return table
+function minescript.entities(nbt)
+	return msrt.call_async("entities", nbt)
+end
+
+---Gets world properties.
+---@async
+---@return table
+function minescript.world_properties()
+	return msrt.call_async("world_properties")
+end
+
+---Gets the type of block at position (x, y, z).
+---@async
+---@param x number
+---@param y number
+---@param z number
+---@return table
+function minescript.getblock(x, y, z)
+	return msrt.call_async("getblock", x, y, z)
+end
+
+---Gets the types of block at the specified [x, y, z] positions.
+---@async
+---@param positions table
+---@return table
+function minescript.getblocklist(positions)
+	return msrt.call_async("getblocklist", positions)
+end
+
+---Notifies the caller when the region from (x1, z1) to (x2, z2) is loaded.
+---@async
+---@param x1 number
+---@param z1 number
+---@param x2 number
+---@param z2 number
+function minescript.await_loaded_region(x1, z1, x2, z2)
+	msrt.call_async("await_loaded_region", x1, z1, x2, z2)
+end
+
+---Registers a listener for receiving keyboard events. One listener allowed per job.
+---@param listener function
+---@param exception_handler function?
+function minescript.register_key_event_listener(listener, exception_handler)
+	msrt.add_task("register_key_event_listener", nil, listener, exception_handler)
+end
+
+---Unregisters a key event listener, if any, for the currently running job.
+---@async
+function minescript.unregister_key_event_listener()
+	msrt.call_async("unregister_key_event_listener")
+end
+
+msrt.hooks.unregister_key_event_listener = function()
+	for k, v in pairs(msrt.calls) do
+		if v[1] == "register_key_event_listener" then
+			msrt.calls[k] = nil
+		end
+	end
+end
+
+---Registers a listener for receiving chat messages. One listener allowed per job.
+---@param listener function
+---@param exception_handler function?
+function minescript.register_chat_message_listener(listener, exception_handler)
+	msrt.add_task("register_chat_message_listener", nil, listener, exception_handler)
+end
+
+---Unregisters a chat message listener, if any, for the currently running job.
+---@async
+function minescript.unregister_chat_message_listener()
+	msrt.call_async("unregister_chat_message_listener")
+end
+
+---Registers an interceptor for swallowing chat messages.
+---@param interceptor function
+---@param exception_handler function?
+function minescript.register_chat_message_interceptor(interceptor, exception_handler)
+	msrt.add_task("register_chat_message_interceptor", nil, interceptor, exception_handler)
+end
+
+---Unregisters the chat message interceptor, if one is currently registered.
+---@async
+function minescript.unregister_chat_message_interceptor()
+	msrt.call_async("unregister_chat_message_interceptor")
+end
+
+---Gets the current GUI screen name, if there is one.
+---@async
+function minescript.screen_name()
+	msrt.call_async("screen_name")
+end
+
+return minescript
---
+++ minescript/msrt.lua
@@ -0,0 +1,149 @@
+local json = require("json")
+
+local msrt = {}
+
+---@param s string
+local function print_err(s)
+	io.stderr:write(tostring(s) .. "\n")
+end
+
+---@param f function
+---@param ... any?
+---@return thread
+local function call(f, ...)
+	assert(not coroutine.running(), "attempt to call a function from a coroutine")
+	return coroutine.wrap(f)(...)
+end
+
+---@param f function
+---@return function
+local function wrap(f)
+	return function(...)
+		return call(f, ...)
+	end
+end
+
+local _next_fcallid = 1000
+msrt.calls = {}
+msrt.hooks = {}
+
+---Calls a script function, asynchronously streaming return value(s).
+---@param func_name string name of Minescript function to call
+---@param args table?
+---@param retval_handler function callback invoked for each return value
+---@param exception_handler function?
+local function add_task(func_name, args, retval_handler, exception_handler)
+	retval_handler = wrap(retval_handler)
+	exception_handler = exception_handler and wrap(exception_handler)
+
+	_next_fcallid = _next_fcallid + 1
+	msrt.calls[_next_fcallid] = {func_name, retval_handler, exception_handler}
+	print(("?%s %s %s"):format(_next_fcallid, func_name, json.encode(args or {})))
+
+	local hook = msrt.hooks[func_name]
+	if hook then
+		hook()
+	end
+end
+
+---Calls a script function and returns the function's return value.
+---@param func_name string name of Minescript function to call
+---@param ... any
+---@return any ret script function's return value: number, string, list, or dict
+local function call_async(func_name, ...)
+	local c = assert(coroutine.running(), "attempt to yield from outside a coroutine")
+
+	local function ret(retval)
+		assert(coroutine.resume(c, retval))
+	end
+
+	add_task(func_name, {...}, ret, error)
+
+	return coroutine.yield()
+end
+
+local function service_step()
+	local json_input = io.read()
+	local ok, reply = xpcall(json.decode, debug.traceback, json_input)
+	if not ok then
+		print_err(reply)
+		print_err(("JSON error in: %s"):format(json_input))
+		return
+	end
+
+	local func_call_id = reply.fcid
+	if not func_call_id then
+		print_err("'fcid' field missing in script function response")
+		return
+	end
+
+	if func_call_id == 0 then
+		if reply.retval == "exit!" then
+			return true
+		end
+		return
+	end
+
+	if not msrt.calls[func_call_id] then
+		print_err(("fcid=%s not found in calls"):format(
+			func_call_id
+		))
+		return
+	end
+
+	local func_name, retval_handler, exception_handler = unpack(msrt.calls[func_call_id])
+	if reply.conn == "close" then
+		msrt.calls[func_call_id] = nil
+	end
+
+	if reply.except then
+		local exception_type = reply.except.type
+		local message = reply.except.message
+		if not exception_handler then
+			print_err(("%s in %s: %s"):format(exception_type, func_name, message))
+		else
+			exception_handler(("%s(%q)"):format(exception_type, message))
+		end
+	elseif reply.retval then
+		retval_handler(reply.retval)
+	end
+
+	if not reply.conn and not reply.retval and not reply.except then
+		print_err(("script function response missing 'conn', 'retval', and 'except': %s"):format(
+			json_input
+		))
+	end
+end
+
+---@param f function
+---@param ... any
+local function run(f, ...)
+	local done = false
+
+	wrap(function(...)
+		f()
+		done = true
+	end)()
+
+	if done then
+		return
+	end
+
+	while true do
+		if service_step() then
+			return
+		end
+		if done and not next(msrt.calls) then
+			print_err("quitting")
+			-- print("?0 exit!")  -- bugged, need delay?
+			return
+		end
+	end
+end
+
+msrt.add_task = add_task
+msrt.call_async = call_async
+msrt.print_err = print_err
+msrt.run = run
+
+return msrt
